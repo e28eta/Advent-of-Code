b347de72a569b0959f365d2e5a983b348cf08ab9
Daniel Jackson <dan@djackson.org>
2018-01-01 14:29:24 -0800

    Day 12 part 2

--- 2017 Advent of Code.playground/Pages/Day 12.xcplaygroundpage/Contents.swift
+++ 2017 Advent of Code.playground/Pages/Day 12.xcplaygroundpage/Contents.swift
@@ -66,17 +66,35 @@ struct ProgramGraph {
     }
 
     func neighborCount(of start: Int) -> Int {
-        var seen: Set<Int> = [start]
-        var queue: [Int] = [start]
+        return group(containing: start).count
+    }
+
+    func group(containing id: Int) -> Set<Int> {
+        var group: Set<Int> = [id]
+        var queue: [Int] = [id]
 
         while let nextId = queue.popLast(), let next = graph[nextId] {
-            let newNeighbors = next.connections.filter { !seen.contains($0) }
+            let newNeighbors = next.connections.filter { !group.contains($0) }
 
             queue.append(contentsOf: newNeighbors)
-            seen.formUnion(newNeighbors)
+            group.formUnion(newNeighbors)
         }
 
-        return seen.count
+        return group
+    }
+
+    func allGroups() -> [Set<Int>] {
+        var groups: [Set<Int>] = []
+        var allIds = Set(graph.keys)
+
+        while let nextId = allIds.popFirst() {
+            let nextGroup = group(containing: nextId)
+
+            allIds.subtract(nextGroup)
+            groups.append(nextGroup)
+        }
+
+        return groups
     }
 }
 
@@ -100,16 +118,31 @@ let graph = ProgramGraph(input)
 assertEqual(graph.neighborCount(of: 0), 380)
 
 /*:
- # Part Two ---
+ # Part Two
 
- There are more programs than just the ones in the group containing program ID 0. The rest of them have no way of reaching that group, and still might have no way of reaching each other.
+ There are more programs than just the ones in the group containing program ID `0`. The rest of them have no way of reaching that group, and still might have no way of reaching each other.
 
- A group is a collection of programs that can all communicate via pipes either directly or indirectly. The programs you identified just a moment ago are all part of the same group. Now, they would like you to determine the total number of groups.
+ A **group** is a collection of programs that can all communicate via pipes either directly or indirectly. The programs you identified just a moment ago are all part of the same group. Now, they would like you to determine the total number of groups.
 
- In the example above, there were 2 groups: one consisting of programs 0,2,3,4,5,6, and the other consisting solely of program 1.
+ In the example above, there were 2 groups: one consisting of programs `0,2,3,4,5,6`, and the other consisting solely of program `1`.
 
- How many groups are there in total?
+ **How many groups** are there in total?
  */
+
+let testData2 = [
+    ("""
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+""", 2),
+]
+
+verify(testData2) { ProgramGraph($0).allGroups().count }
+assertEqual(graph.allGroups().count, 181)
 
 
 //: [Next](@next)
