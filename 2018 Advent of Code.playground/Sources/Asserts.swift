1f1aec1ebd0f85e8cf2c28c5a189d6c2998fcadb
Daniel Jackson <dan@djackson.org>
2018-12-01 11:33:03 -0800

    Return success/failure from verify
    Haven't found the output in the iPad playgrounds app yet, this should
    help tell whether i'm passing/failing

--- 2018 Advent of Code.playground/Sources/Asserts.swift
+++ 2018 Advent of Code.playground/Sources/Asserts.swift
@@ -4,7 +4,7 @@ public func assertEqual<T>(_ expression1: @autoclosure () throws -> T?,
                            _ expression2: @autoclosure () throws -> T?,
                            _ message: @autoclosure () -> String = "",
                            file: StaticString = #file,
-                           line: UInt = #line) where T: Equatable {
+                           line: UInt = #line) -> Bool where T: Equatable {
     do {
         let (value1, value2) = (try expression1(), try expression2())
 
@@ -14,20 +14,25 @@ public func assertEqual<T>(_ expression1: @autoclosure () throws -> T?,
             preconditionFailure(message,
                 file: file,
                 line: line)
+            return false
         }
+        return true
     } catch {
         print("assertEqual failed: Error thrown while calculating expression \(error)")
         preconditionFailure("Error thrown while calculating expression \(error)",
             file: file,
             line: line)
+        return false
     }
 }
 
-public func verify<T, U>(_ testData: [(T, U)], _ closure: (T) -> U) where U: Equatable {
+public func verify<T, U>(_ testData: [(T, U)], _ closure: (T) -> U) -> Bool where U: Equatable {
+    var succeeded = true
     for (input, expected) in testData {
         let actual = closure(input)
         print(".", terminator: "")
-        assertEqual(actual, expected)
+        succeeded &= assertEqual(actual, expected)
     }
     print()
+    return succeeded
 }
