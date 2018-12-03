5e66dc8cae21c82e1d1ed8b61d9904b2798f141e
Daniel Jackson <dan@djackson.org>
2018-12-03 00:46:17 -0800

    wip day 3
    Solution is too slow on iPad playgrounds, generates too many
    intermediate values.
    
    Going to remove unnecessary optimization that only considers the
    contended area in the Fabric, and move structs to Sources

--- 2018 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
+++ 2018 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
@@ -41,3 +41,131 @@
  
  If the Elves all proceed with their own plans, none of them will have enough fabric. How many square inches of fabric are within two or more claims?
  */
+
+struct Point: CustomStringConvertible { 
+    var x: Int, y: Int
+    
+    init(x: Int, y: Int) { self.x = x; self.y = y }
+    init(_ s: String) {
+        let components = s.split(separator: ",")
+        x = Int(components[0])!
+        y = Int(components[1])!
+    }
+    var description: String { return "\(x), \(y)" }
+}
+
+struct Size: CustomStringConvertible {
+    let width: Int, height: Int
+    init(width: Int, height: Int) { self.width = width; self.height = height }
+    init(_ s: String) {
+        let components = s.split(separator: "x")
+        width = Int(components[0])!
+        height = Int(components[1])!
+    }
+    var description: String { return "\(width) x \(height)" }
+}
+
+struct Claim {
+    let id: String
+    let origin: Point
+    let size: Size
+    
+    init(_ s: String) {
+        var s = s.replacingOccurrences(of: "#", with: "")
+        var components = s.components(separatedBy: " @ ")
+        id = components[0]
+        components = components[1].components(separatedBy: ": ")
+        origin = Point(components[0])
+        size = Size(components[1])
+    }
+}
+
+struct Fabric { 
+    var area: [[Int]]
+    
+    init(_ size: Size) {
+        let column = Array(repeating: 0, count: size.height)
+        area = Array(repeating: column, count: size.width)
+    }
+    
+    subscript(x: Int, y: Int) -> Int {
+        get {
+            return area[x][y]
+        }
+        set(newValue) {
+            area[x][y] = newValue
+        }
+    }
+    
+    mutating func add(claim: Claim) {
+        for x in (claim.origin.x ..< claim.origin.x + claim.size.width) {
+            for y in (claim.origin.y ..< claim.origin.y + claim.size.height) {
+                self[x, y] += 1
+            }
+        }
+    }
+    
+    var contendedFabric: Int {
+        return area.reduce(0) { result, column in
+            column.reduce(result) { r, v in
+                r + (v > 1 ? 1 : 0)
+            }
+        }
+    }
+}
+
+let testData = [
+    ("#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2".lines().map(Claim.init), 4),
+]
+let input = try readResourceFile("input.txt").lines().map(Claim.init)
+
+func contendedArea(_ claims: [Claim]) -> (min: Point, max: Point) {
+    var minPoint = Point(x: Int.max, y: Int.max)
+    var maxPoint = Point(x: Int.min, y: Int.min)
+    var contendedMin = minPoint, contendedMax = maxPoint
+    
+    for claim in claims {
+        if claim.origin.x < minPoint.x {
+            contendedMin.x = minPoint.x
+            minPoint.x = claim.origin.x
+        } else if claim.origin.x < contendedMin.x {
+            contendedMin.x = claim.origin.x
+        }
+        if claim.origin.y < minPoint.y {
+            contendedMin.y = minPoint.y
+            minPoint.y = claim.origin.y
+        } else if claim.origin.y < contendedMin.y {
+            contendedMin.y = claim.origin.y
+        }
+        
+        if claim.origin.x + claim.size.width > maxPoint.x {
+            contendedMax.x = maxPoint.x
+            maxPoint.x = claim.origin.x + claim.size.width
+        } else if claim.origin.x + claim.size.width > contendedMax.x {
+            contendedMax.x = claim.origin.x + claim.size.width
+        }
+        if claim.origin.y + claim.size.height > maxPoint.y {
+            contendedMax.y = maxPoint.y
+            maxPoint.y = claim.origin.y + claim.size.height
+        } else if claim.origin.y + claim.size.height > contendedMax.y {
+            contendedMax.y = claim.origin.y + claim.size.height
+        }
+    }
+    
+    return (min: contendedMin, max: contendedMax)
+}
+
+func partOne(_ claims: [Claim]) -> Int {
+    let contention = contendedArea(claims)
+    
+    var fabric = Fabric(Size(width: contention.max.x + 1, height: contention.max.y + 1))
+    for claim in claims { fabric.add(claim: claim) }
+
+    return fabric.contendedFabric
+}
+
+verify(testData, partOne)
+
+partOne(input)
+
+
