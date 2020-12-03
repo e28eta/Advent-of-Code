b78cc6a7294cc2b03320537396d0fa7c898c0b2f
Dan Jackson <dan@djackson.org>
2020-12-03 00:38:46 -0800

    2020 day three part two

--- 2020 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
+++ 2020 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
@@ -123,8 +123,41 @@ let exampleInput = Slope("""
 assertEqual(exampleInput.path().filter({ $0 == .tree}).count, 7)
 
 let slope = try Slope(readResourceFile("input.txt").lines())
-
 assertEqual(slope.path().filter({ $0 == .tree}).count, 259)
 
+
+/**
+ --- Part Two ---
+
+ Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+ Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+ Right 1, down 1.
+ Right 3, down 1. (This is the slope you already checked.)
+ Right 5, down 1.
+ Right 7, down 1.
+ Right 1, down 2.
+ In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+ What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+ */
+
+verify([
+    (exampleInput, 336),
+    (slope, 2224913600),
+]) { (slope) in
+    let angles = [
+        (1, 1),
+        (1, 3),
+        (1, 5),
+        (1, 7),
+        (2, 1)
+    ]
+
+    return angles.map { angle in
+        slope.path(down: angle.0, right: angle.1).filter({ $0 == .tree}).count
+    }.reduce(1) { $0 * $1 }
+}
 
 //: [Next](@next)
