c0dac56ca5cb1fee3b5356e0e266167928f8f726
Dan Jackson <dan@djackson.org>
2020-12-06 22:25:41 -0800

    2020 day seven part two
    definitely some extra/unnecessary guard rails, but nice to protect myself.

--- 2020 Advent of Code.playground/Pages/Day 7.xcplaygroundpage/Contents.swift
+++ 2020 Advent of Code.playground/Pages/Day 7.xcplaygroundpage/Contents.swift
@@ -115,4 +115,87 @@ verify([
     canContain(rules: rules).count
 }
 
+/**
+ --- Part Two ---
+
+ It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+
+ Consider again your `shiny gold` bag and the rules from the above example:
+
+ - `faded blue` bags contain `0` other bags.
+ - `dotted black` bags contain `0` other bags.
+ - `vibrant plum` bags contain `11` other bags: 5 `faded blue` bags and 6 `dotted black` bags.
+ - `dark olive` bags contain `7` other bags: 3 `faded blue` bags and 4 `dotted black` bags.
+
+ So, a single `shiny gold` bag must contain 1 `dark olive` bag (and the 7 bags within it) plus 2 `vibrant plum` bags (and the 11 bags within **each** of those): `1 + 1*7 + 2 + 2*11` = `32` bags!
+
+ Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+ Here's another example:
+
+ ```
+ shiny gold bags contain 2 dark red bags.
+ dark red bags contain 2 dark orange bags.
+ dark orange bags contain 2 dark yellow bags.
+ dark yellow bags contain 2 dark green bags.
+ dark green bags contain 2 dark blue bags.
+ dark blue bags contain 2 dark violet bags.
+ dark violet bags contain no other bags.
+ ```
+
+ In this example, a single `shiny gold` bag must contain `126` other bags.
+
+ \**How many individual bags are required inside your single `shiny gold` bag?**
+ */
+
+class BagRules {
+    let bags: [Color: Bag]
+    var requiredContentCount: [Color: Int?] = [:]
+
+    func contentCount(for color: Color) -> Int {
+        switch requiredContentCount[color] {
+        case .some(.some(let count)):
+            return count
+        case .some(.none):
+            fatalError("cycle detected? \(color)")
+        case .none:
+            requiredContentCount[color] = .none // mark as currently calculating this color
+            guard let bag = bags[color] else { fatalError("missing rules for \(color)") }
+
+            let count = bag.capacity.reduce(0) { (sum, keyValue) in
+                let (nextColor, count) = keyValue
+
+                return sum + count + (count * contentCount(for: nextColor))
+            }
+            requiredContentCount[color] = count
+            return count
+        }
+    }
+
+    init(_ rules: [Bag]) {
+        bags = Dictionary(grouping: rules, by: \.color).mapValues {
+            guard $0.count == 1 else { fatalError(">1 rule for \($0.first!.color)")}
+            return $0.first!
+        }
+    }
+}
+
+let example2 = """
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+""".lines().compactMap(Bag.init)
+
+verify([
+    (exampleInput, 32),
+    (example2, 126),
+    (input, 30055)
+]) { bags in
+    BagRules(bags).contentCount(for: "shiny gold")
+}
+
 //: [Next](@next)
