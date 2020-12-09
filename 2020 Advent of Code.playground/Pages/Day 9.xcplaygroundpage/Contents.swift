3bb2805250959a6839cda6c907fbf92f5f48ed2b
Dan Jackson <dan@djackson.org>
2020-12-09 00:09:33 -0800

    2020 day nine part two

--- 2020 Advent of Code.playground/Pages/Day 9.xcplaygroundpage/Contents.swift
+++ 2020 Advent of Code.playground/Pages/Day 9.xcplaygroundpage/Contents.swift
@@ -126,11 +126,74 @@ struct XMAS {
     }
 }
 
+let exampleXmas = XMAS(preambleLength: 5, numbers: example)
 let xmas = XMAS(preambleLength: 25, numbers: input)
 verify([
-    (XMAS(preambleLength: 5, numbers: example), 127),
+    (exampleXmas, 127),
     (xmas, 1721308972),
 ]) { $0.firstNonSum().number }
 
+/**
+ --- Part Two ---
+
+ The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a **contiguous set of at least two numbers** in your list which sum to the invalid number from step 1.
+
+ Again consider the above example:
+
+ ```
+ 35
+ 20
+ 15
+ 25
+ 47
+ 40
+ 62
+ 55
+ 65
+ 95
+ 102
+ 117
+ 150
+ 182
+ 127
+ 219
+ 299
+ 277
+ 309
+ 576
+ ```
+
+ In this list, adding up all of the numbers from `15` through `40` produces the invalid number from step 1, `127`. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+
+ To find the **encryption weakness**, add together the **smallest** and **largest** number in this contiguous range; in this example, these are `15` and `47`, producing `62`.
+
+ What is the encryption weakness in your XMAS-encrypted list of numbers?
+ */
+
+extension XMAS {
+    func encryptionWeakness(invalidNumber: Int) -> Int {
+        var window: [Int] = []
+        var iterator = numbers.makeIterator()
+
+        repeat {
+            let sum = window.reduce(0, +)
+            if sum < invalidNumber {
+                window.append(iterator.next()!)
+            } else if sum == invalidNumber {
+                let sorted = window.sorted()
+                return sorted.first! + sorted.last!
+            } else {
+                window.removeFirst()
+            }
+        } while true
+    }
+}
+
+verify([
+    ((exampleXmas, 127), 62),
+    ((xmas, 1721308972), 209694133),
+]) {
+    $0.0.encryptionWeakness(invalidNumber: $0.1)
+}
 
 //: [Next](@next)
