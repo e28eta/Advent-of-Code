cee80cd75f1a0678c06de6f0ad7f2326052a9c65
Dan Jackson <dan@djackson.org>
2020-12-13 14:30:24 -0800

    2020 day thirteen part two
    Luckily I figured out this was a Diophantine matching the Chinese Remainder
    theorem.
    
    Brute force was clearly out, checking millions of #s destroys the Xcode playground.
    
    So, I started with the bus schedules as a system of linear equations. I don't actually
    know how to solve them with a computer - and was off reading Swift + Accelerate
    documentation, which gets complicated pretty quickly for someone with just a passing
    recollection of linear equations.
    
    Something that became ~fairly obvious is that the fact I'm looking for integer solutions
    was important. googling for that led me to https://en.wikipedia.org/wiki/Diophantine_equation
    and I was pleased to see "Diophantine problems have fewer equations than unknowns and involve finding integers that solve simultaneously all equations.", since I had N+1 unknowns.
    
    Poking through that page, it seemed like the Chinese remainder theorem matched, and I
    chose to use the sieve. I didn't even bother reading the general case solution that
    involves matrices, and ended up not needing it.
    
    It took a little twiddling to figure out the correct value for the `remainder` so that I
    was getting the correct matching sequence, but thanks to the several input examples
    I was able to get the right answer on the first submission.

--- 2020 Advent of Code.playground/Pages/Day 13.xcplaygroundpage/Contents.swift
+++ 2020 Advent of Code.playground/Pages/Day 13.xcplaygroundpage/Contents.swift
@@ -74,16 +74,20 @@ struct Bus {
 }
 
 struct BusSchedule {
-    let earliestDeparture: Int
+    let earliestDeparture: Int?
     let busses: [Bus]
 
     init(_ string: String) {
-        let arr = string.lines()
-        earliestDeparture = Int(arr[0])!
-        busses = arr[1].split(separator: ",").map({ Int($0) }).map(Bus.init)
+        var arr = string.lines()
+        let depart: String? = arr.count > 1 ? arr.removeFirst() : nil
+        let busses = arr.removeFirst()
+
+        self.earliestDeparture = depart.flatMap(Int.init)
+        self.busses = busses.split(separator: ",").map({ Int($0) }).map(Bus.init)
     }
 
     func part1() -> Int {
+        guard let earliestDeparture = earliestDeparture else { fatalError() }
         return busses.filter {
             $0.departureInterval != nil
         }
@@ -110,6 +114,135 @@ verify([
     (input, 3215),
 ]) {
     BusSchedule($0).part1()
+}
+
+/**
+ --- Part Two ---
+
+ The shuttle company is running a contest: one gold coin for anyone that can find the earliest timestamp such that the first bus ID departs at that time and each subsequent listed bus ID departs at that subsequent minute. (The first line in your input is no longer relevant.)
+
+ For example, suppose you have the same list of bus IDs as above:
+
+ ```
+ 7,13,x,x,59,x,31,19
+ ```
+
+ An `x` in the schedule means there are no constraints on what bus IDs must depart at that time.
+
+ This means you are looking for the earliest timestamp (called `t`) such that:
+
+ - Bus ID `7` departs at timestamp `t`.
+ - Bus ID `13` departs one minute after timestamp `t`.
+ - There are no requirements or restrictions on departures at two or three minutes after timestamp `t`.
+ - Bus ID `59` departs four minutes after timestamp `t`.
+ - There are no requirements or restrictions on departures at five minutes after timestamp `t`.
+ - Bus ID `31` departs six minutes after timestamp `t`.
+ - Bus ID `19` departs seven minutes after timestamp `t`.
+
+ The only bus departures that matter are the listed bus IDs at their specific offsets from `t`. Those bus IDs can depart at other times, and other bus IDs can depart at those times. For example, in the list above, because bus ID `19` must depart seven minutes after the timestamp at which bus ID `7` departs, bus ID `7` will always also be departing with bus ID `19` at seven minutes after timestamp `t`.
+
+ In this example, the earliest timestamp at which this occurs is `1068781`:
+
+ ```
+ time     bus 7   bus 13  bus 59  bus 31  bus 19
+ 1068773    .       .       .       .       .
+ 1068774    D       .       .       .       .
+ 1068775    .       .       .       .       .
+ 1068776    .       .       .       .       .
+ 1068777    .       .       .       .       .
+ 1068778    .       .       .       .       .
+ 1068779    .       .       .       .       .
+ 1068780    .       .       .       .       .
+ 1068781    D       .       .       .       .
+ 1068782    .       D       .       .       .
+ 1068783    .       .       .       .       .
+ 1068784    .       .       .       .       .
+ 1068785    .       .       D       .       .
+ 1068786    .       .       .       .       .
+ 1068787    .       .       .       D       .
+ 1068788    D       .       .       .       D
+ 1068789    .       .       .       .       .
+ 1068790    .       .       .       .       .
+ 1068791    .       .       .       .       .
+ 1068792    .       .       .       .       .
+ 1068793    .       .       .       .       .
+ 1068794    .       .       .       .       .
+ 1068795    D       D       .       .       .
+ 1068796    .       .       .       .       .
+ 1068797    .       .       .       .       .
+ ```
+
+ In the above example, bus ID `7` departs at timestamp `1068788` (seven minutes after `t`). This is fine; the only requirement on that minute is that bus ID `19` departs then, and it does.
+
+ Here are some other examples:
+
+ - The earliest timestamp that matches the list `17,x,13,19` is `3417`.
+ - `67,7,59,61` first occurs at timestamp `754018`.
+ - `67,x,7,59,61` first occurs at timestamp `779210`.
+ - `67,7,x,59,61` first occurs at timestamp `1261476`.
+ - `1789,37,47,1889` first occurs at timestamp `1202161486`.
+
+ However, with so many bus IDs in your list, surely the actual earliest timestamp will be larger than `100000000000000`!
+
+ \**What is the earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the list?**
+
+ */
+
+extension BusSchedule {
+    func part2() -> Int {
+        let constraints = self.busses.enumerated().compactMap { e -> (modulo: Int, remainder: Int)? in
+            guard let interval = e.element.departureInterval else { return nil }
+            var remainder = e.offset % interval
+            if remainder != 0 {
+                remainder = interval - remainder
+            }
+            return (modulo: interval, remainder: remainder)
+        }
+
+        return chineseRemainderSieve(constraints: constraints)
+    }
+}
+
+struct IntegerStepSequence: Sequence {
+    typealias Element = Int
+    let start: Int
+    let step: Int
+
+    func makeIterator() -> AnyIterator<Int> {
+        var value = start
+        return AnyIterator<Int> {
+            value += step
+            return value
+        }
+    }
+}
+
+/// https://en.wikipedia.org/wiki/Chinese_remainder_theorem#Search_by_sieving
+func chineseRemainderSieve(constraints: [(modulo: Int, remainder: Int)]) -> Int {
+    // largest to smallest modulos
+    let sortedConstraints = constraints.sorted { $0.modulo > $1.modulo }
+    guard let largestModulo = sortedConstraints.first else { return -1 }
+    let initialSequence = IntegerStepSequence(start: largestModulo.remainder, step: largestModulo.modulo)
+
+    return sortedConstraints.dropFirst().reduce(initialSequence) { sequence, constraint in
+        let match = sequence.first { n in
+            n % constraint.modulo == constraint.remainder
+        }! // infinite sequence will find a match
+        return IntegerStepSequence(start: match, step: sequence.step * constraint.modulo)
+    }.start
+}
+
+
+verify([
+    (exampleInput, 1068781),
+    ("17,x,13,19", 3417),
+    ("67,7,59,61", 754018),
+    ("67,x,7,59,61", 779210),
+    ("67,7,x,59,61", 1261476),
+    ("1789,37,47,1889", 1202161486),
+    (input, 1001569619313439),
+]) {
+    BusSchedule($0).part2()
 }
 
 //: [Next](@next)
