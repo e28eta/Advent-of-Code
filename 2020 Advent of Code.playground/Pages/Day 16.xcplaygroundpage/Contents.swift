79035eb5c9621ecfb4b80adef67e3886d5f6ef30
Dan Jackson <dan@djackson.org>
2020-12-16 00:21:40 -0800

    2020 day sixteen part two
    Looks like only one of the fields is definitively identified, and it then needs to loop
    ~19 times to figure out which fields are subsequently determined. Ex: 0 must be A, 1 might
    be A or B, ergo 1 has to be B, etc.
    
    I know there's a faster way to do this, but this was definitely fast enough.
    
    The hotspot is actually the ~60k comparisons of every value against every rule to figure
    out which rules could not apply for each ticket position. I don't think there's any way
    to avoid that work.
    
    I moved *some* shared work into the TicketDocument constructor, instead of repeating it
    for p1 and p2, but I think that was probably negligible and not worth the effort.

--- 2020 Advent of Code.playground/Pages/Day 16.xcplaygroundpage/Contents.swift
+++ 2020 Advent of Code.playground/Pages/Day 16.xcplaygroundpage/Contents.swift
@@ -58,7 +58,7 @@ struct Ticket {
     }
 }
 
-struct Rule {
+struct Rule: Hashable {
     let label: String
     let validValues: MultipleClosedRanges<Int>
 
@@ -73,32 +73,48 @@ struct Rule {
         }
         validValues = MultipleClosedRanges(ranges)
     }
+
+    static func == (lhs: Rule, rhs: Rule) -> Bool {
+        // good enough
+        return lhs.label == rhs.label
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(label)
+    }
 }
 
 struct TicketDocument {
     let rules: [Rule]
     let myTicket: Ticket
     let nearbyTickets: [Ticket]
+    let validNearbyTickets: [Ticket]
+    let scanningErrorRate: Int
 
     init?(_ string: String) {
         let arr = string.components(separatedBy: "\n\nyour ticket:\n")
         guard arr.count == 2 else { return nil }
-
-        rules = arr[0].lines().map(Rule.init)
-
         let tickets = arr[1].components(separatedBy: "\n\nnearby tickets:\n")
         guard tickets.count == 2 else { return nil }
 
+        rules = arr[0].lines().map(Rule.init)
         myTicket = tickets[0].lines().map(Ticket.init).first!
         nearbyTickets = tickets[1].lines().map(Ticket.init)
+
+        let allValidValues = MultipleClosedRanges(rules.flatMap(\.validValues.combinedRanges))
+
+        validNearbyTickets = nearbyTickets.filter { (ticket) in
+            ticket.values.allSatisfy(allValidValues.contains)
+        }
+
+        scanningErrorRate = nearbyTickets.lazy
+            .flatMap(\.values)
+            .filter { !allValidValues.contains($0) }
+            .reduce(0, +)
     }
 
     func ticketScanningErrorRate() -> Int {
-        let allValidValues = MultipleClosedRanges(rules.flatMap(\.validValues.combinedRanges))
-
-        return nearbyTickets.flatMap(\.values)
-            .filter { !allValidValues.contains($0) }
-            .reduce(0, +)
+        return scanningErrorRate
     }
 }
 
@@ -133,7 +149,7 @@ struct MultipleClosedRanges<U> where U: Strideable {
     }
 }
 
-let exampleInput = """
+let exampleInput = TicketDocument("""
 class: 1-3 or 5-7
 row: 6-11 or 33-44
 seat: 13-40 or 45-50
@@ -146,15 +162,108 @@ nearby tickets:
 40,4,50
 55,2,20
 38,6,12
-"""
-let input = try readResourceFile("input.txt")
+""")
+let input = try TicketDocument(readResourceFile("input.txt"))
 
 
 verify([
-(exampleInput, 71),
-(input, 19087),
+    (exampleInput, 71),
+    (input, 19087),
 ]) {
-    TicketDocument($0)?.ticketScanningErrorRate() ?? -1
+    $0?.ticketScanningErrorRate() ?? -1
+}
+
+/**
+ --- Part Two ---
+
+ Now that you've identified which tickets contain invalid values, **discard those tickets entirely.** Use the remaining valid tickets to determine which field is which.
+
+ Using the valid ranges for each field, determine what order the fields appear on the tickets. The order is consistent between all tickets: if seat is the third field, it is the third field on every ticket, including **your ticket.**
+
+ For example, suppose you have the following notes:
+
+ ```
+ class: 0-1 or 4-19
+ row: 0-5 or 8-19
+ seat: 0-13 or 16-19
+
+ your ticket:
+ 11,12,13
+
+ nearby tickets:
+ 3,9,18
+ 15,1,5
+ 5,14,9
+ ```
+
+ Based on the **nearby tickets** in the above example, the first position must be `row`, the second position must be `class`, and the third position must be `seat`; you can conclude that in **your ticket**, `class` is `12`, `row` is `11`, and `seat` is `13`.
+
+ Once you work out which field is which, look for the six fields on **your ticket** that start with the word `departure`. **What do you get if you multiply those six values together?**
+ */
+
+extension TicketDocument {
+    func fieldOrdering() -> [Rule] {
+        var possibleFieldOrderings = myTicket.values.indices.map { index -> Set<Rule> in
+            rules.filter { rule in
+                // this rule could apply if every valid ticket conforms to it
+                validNearbyTickets.allSatisfy { ticket in
+                    rule.validValues.contains(ticket.values[index])
+                }
+            }.reduce(into: Set()) {
+                $0.insert($1)
+            }
+        }
+
+        // presumably now I have some Rules that've been uniquely identified,
+        // use those to figure out what's no longer possible for the rest
+        repeat {
+            let definitivelyIdentifiedRules = possibleFieldOrderings.reduce(into: Set<Rule>()) { set, rules in
+                guard rules.count == 1, let rule = rules.first else { return }
+                set.insert(rule)
+            }
+
+            possibleFieldOrderings = possibleFieldOrderings.map {
+                guard $0.count > 1 else { return $0 }
+                return $0.subtracting(definitivelyIdentifiedRules)
+            }
+        } while (!possibleFieldOrderings.allSatisfy({ $0.count == 1 }))
+
+        return possibleFieldOrderings.flatMap { $0 }
+    }
+
+    func partTwo() -> Int {
+        return fieldOrdering()
+            .enumerated()
+            .filter { $0.element.label.hasPrefix("departure") }
+            .map { myTicket.values[$0.offset] }
+            .reduce(1, *)
+    }
+}
+
+let exampleTwo = TicketDocument("""
+class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+""")
+
+verify([
+    (exampleTwo, ["row", "class", "seat"]),
+]) {
+    $0?.fieldOrdering().map(\.label) ?? [String]()
+}
+
+verify([
+    (input, 1382443095281)
+]) {
+    $0?.partTwo() ?? -1
 }
 
 //: [Next](@next)
