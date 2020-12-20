e08d2ec3053147a37ef95c378b9a42724b98f24f
Dan Jackson <dan@djackson.org>
2020-12-20 13:46:33 -0800

    2020 day nineteen p2 prompt, no solution

--- 2020 Advent of Code.playground/Pages/Day 19.xcplaygroundpage/Contents.swift
+++ 2020 Advent of Code.playground/Pages/Day 19.xcplaygroundpage/Contents.swift
@@ -96,20 +96,20 @@ indirect enum Rule {
                 rules[ruleNumber]!.nfaStates(rules)
             }
             .reduce(start) { (previous, nfa) in
-                previous.transition = .unlabeled(nfa.start, nil)
+                previous.transition = .unlabeled(nfa.start)
                 return nfa.final
             }
-            .transition = .unlabeled(final, nil)
+            .transition = .unlabeled(final)
         case .alternatives(let optionANumbers, let optionBNumbers):
             let optionAStates = optionANumbers.map { rules[$0]!.nfaStates(rules) }
             let optionBStates = optionBNumbers.map { rules[$0]!.nfaStates(rules) }
 
             optionAStates.reversed().reduce(final) { previous, state in
-                state.final.transition = .unlabeled(previous, nil)
+                state.final.transition = .unlabeled(previous)
                 return state.start
             }
             optionBStates.reversed().reduce(final) { previous, state in
-                state.final.transition = .unlabeled(previous, nil)
+                state.final.transition = .unlabeled(previous)
                 return state.start
             }
             start.transition = .unlabeled(optionAStates.first!.start, optionBStates.first!.start)
@@ -124,7 +124,7 @@ class NFAState: Hashable {
 
     enum Edge {
         case none
-        case unlabeled(NFAState, NFAState?)
+        case unlabeled(NFAState, NFAState? = nil)
         case labeled(Character, NFAState)
     }
 
@@ -311,6 +311,90 @@ verify([
         $0 + (validator.validate($1) ? 1 : 0)
     }
 }
+
+/**
+ --- Part Two ---
+
+ As you look over the list of messages, you realize your matching rules aren't quite right. To fix them, completely replace rules 8: 42 and 11: 42 31 with the following:
+
+ 8: 42 | 42 8
+ 11: 42 31 | 42 11 31
+ This small change has a big impact: now, the rules do contain loops, and the list of messages they could hypothetically match is infinite. You'll need to determine how these changes affect which messages are valid.
+
+ Fortunately, many of the rules are unaffected by this change; it might help to start by looking at which rules always match the same set of values and how those rules (especially rules 42 and 31) are used by the new versions of rules 8 and 11.
+
+ (Remember, you only need to handle the rules you have; building a solution that could handle any hypothetical combination of rules would be significantly more difficult.)
+
+ For example:
+
+ 42: 9 14 | 10 1
+ 9: 14 27 | 1 26
+ 10: 23 14 | 28 1
+ 1: "a"
+ 11: 42 31
+ 5: 1 14 | 15 1
+ 19: 14 1 | 14 14
+ 12: 24 14 | 19 1
+ 16: 15 1 | 14 14
+ 31: 14 17 | 1 13
+ 6: 14 14 | 1 14
+ 2: 1 24 | 14 4
+ 0: 8 11
+ 13: 14 3 | 1 12
+ 15: 1 | 14
+ 17: 14 2 | 1 7
+ 23: 25 1 | 22 14
+ 28: 16 1
+ 4: 1 1
+ 20: 14 14 | 1 15
+ 3: 5 14 | 16 1
+ 27: 1 6 | 14 18
+ 14: "b"
+ 21: 14 1 | 1 14
+ 25: 1 1 | 1 14
+ 22: 14 14
+ 8: 42
+ 26: 14 22 | 1 20
+ 18: 15 15
+ 7: 14 5 | 1 21
+ 24: 14 1
+
+ abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+ bbabbbbaabaabba
+ babbbbaabbbbbabbbbbbaabaaabaaa
+ aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+ bbbbbbbaaaabbbbaaabbabaaa
+ bbbababbbbaaaaaaaabbababaaababaabab
+ ababaaaaaabaaab
+ ababaaaaabbbaba
+ baabbaaaabbaaaababbaababb
+ abbbbabbbbaaaababbbbbbaaaababb
+ aaaaabbaabaaaaababaa
+ aaaabbaaaabbaaa
+ aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+ babaaabbbaaabaababbaabababaaab
+ aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+ Without updating rules 8 and 11, these rules only match three messages: bbabbbbaabaabba, ababaaaaaabaaab, and ababaaaaabbbaba.
+
+ However, after updating rules 8 and 11, a total of 12 messages match:
+
+ bbabbbbaabaabba
+ babbbbaabbbbbabbbbbbaabaaabaaa
+ aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+ bbbbbbbaaaabbbbaaabbabaaa
+ bbbababbbbaaaaaaaabbababaaababaabab
+ ababaaaaaabaaab
+ ababaaaaabbbaba
+ baabbaaaabbaaaababbaababb
+ abbbbabbbbaaaababbbbbbaaaababb
+ aaaaabbaabaaaaababaa
+ aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+ aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+ After updating rules 8 and 11, how many messages completely match rule 0?
+ */
+
+// I think this is maybe not a big deal - just adding support for * aka "closure" aka repeated elements
+// actually, wrong. This changes the language into a context-free one, since rule 11 requires matching
 
 
 //: [Next](@next)
