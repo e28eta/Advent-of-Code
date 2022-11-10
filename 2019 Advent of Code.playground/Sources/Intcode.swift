2db1b12f86a154a78c0efd9ad15dbd64541f8e04
Dan Jackson <dan@djackson.org>
2022-11-10 00:56:36 -0800

    2019 Day 5 part one
    I have jumped to the next Intcode problem, because thats fun

--- 2019 Advent of Code.playground/Sources/Intcode.swift
+++ 2019 Advent of Code.playground/Sources/Intcode.swift
@@ -1,16 +1,22 @@
 import Foundation
 
+public typealias MemoryAddress = Int
+public typealias Value = Int
+
 public struct IntcodeComputer {
     public private(set) var running: Bool
-    public private(set) var instructionPointer: Int
+    public private(set) var instructionPointer: MemoryAddress
 
-    public private(set) var memory: [Int]
+    public private(set) var memory: [Value]
+
+    public var input: IndexingIterator<Array<Value>>?
+    public private(set) var output: [Value] = []
 
     public init(_ inputString: String) {
         self.init(inputString.split(separator: ",").compactMap(Int.init))
     }
 
-    public init<S: Sequence<Int>>(_ initial: S) {
+    public init<S: Sequence<Value>>(_ initial: S) {
         self.running = true
         self.instructionPointer = 0
         self.memory = Array(initial)
@@ -27,9 +33,14 @@ public struct IntcodeComputer {
 
         switch op {
         case let .add(left: left, right: right, destination: destination):
-            memory[destination] = memory[left] + memory[right]
+            memory[destination] = value(for: left) + value(for: right)
         case let .multiply(left: left, right: right, destination: destination):
-            memory[destination] = memory[left] * memory[right]
+            memory[destination] = value(for: left) * value(for: right)
+        case let .input(destination):
+            guard let val = input?.next() else { fatalError("not enough input") }
+            memory[destination] = val
+        case let .output(source):
+            output.append(value(for: source))
         case .halt:
             running = false
         }
@@ -39,11 +50,7 @@ public struct IntcodeComputer {
         }
     }
 
-    public mutating func fix(_ modifications: (inout [Int]) -> ()) {
-        modifications(&self.memory)
-    }
-
-    public mutating func set(noun: Int, verb: Int) {
+    public mutating func set(noun: Value, verb: Value) {
         precondition((0...99).contains(noun))
         precondition((0...99).contains(verb))
 
@@ -51,25 +58,63 @@ public struct IntcodeComputer {
         memory[2] = verb
     }
 
-    public func read(_ address: Int) -> Int {
+    public func read(_ address: MemoryAddress) -> Value {
         return memory[address]
+    }
+
+    private func value(for parameter: IntcodeOperator.Parameter) -> Value {
+        switch parameter {
+        case .position(let int):
+            return memory[int]
+        case .immediate(let int):
+            return int
+        }
     }
 }
 
 enum IntcodeOperator {
-    case add(left: Int, right: Int, destination: Int)
-    case multiply(left: Int, right: Int, destination: Int)
+    enum Parameter {
+        /// the value stored in memory at the Int
+        case position(MemoryAddress)
+        /// the value of the Int
+        case immediate(Value)
+    }
+
+    /// add left and right, store into destination
+    case add(left: Parameter, right: Parameter, destination: MemoryAddress)
+    /// multiply left and right, store into destination
+    case multiply(left: Parameter, right: Parameter, destination: MemoryAddress)
+    /// read one input value, store into destination
+    case input(destination: MemoryAddress)
+    /// write value from source to the output
+    case output(source: Parameter)
+    /// Stop running
     case halt
 
     init<S: Sequence<Int>>(_ memory: S) throws {
         var memoryIterator = memory.makeIterator()
 
-        let opCode = memoryIterator.next()
-        switch opCode {
+        var opCode = memoryIterator.next()!
+
+        // figure out modes, greedily
+        let p1Mode, p2Mode: Int
+//        (p3Mode, opCode) = opCode.quotientAndRemainder(dividingBy: 10_000)
+        (p2Mode, opCode) = opCode.quotientAndRemainder(dividingBy: 1_000)
+        (p1Mode, opCode) = opCode.quotientAndRemainder(dividingBy: 100)
+
+        switch (opCode) {
         case 1:
-            self = .add(left: memoryIterator.next()!, right: memoryIterator.next()!, destination: memoryIterator.next()!)
+            self = .add(left: Parameter(memoryIterator.next()!, mode: p1Mode),
+                        right: Parameter(memoryIterator.next()!, mode: p2Mode),
+                        destination: memoryIterator.next()!)
         case 2:
-            self = .multiply(left: memoryIterator.next()!, right: memoryIterator.next()!, destination: memoryIterator.next()!)
+            self = .multiply(left: Parameter(memoryIterator.next()!, mode: p1Mode),
+                             right: Parameter(memoryIterator.next()!, mode: p2Mode),
+                             destination: memoryIterator.next()!)
+        case 3:
+            self = .input(destination: memoryIterator.next()!)
+        case 4:
+            self = .output(source: Parameter(memoryIterator.next()!, mode: p1Mode))
         case 99:
             self = .halt
         default:
@@ -80,7 +125,21 @@ enum IntcodeOperator {
     var numberOfValues: Int {
         switch self {
         case .add, .multiply: return 4
+        case .input, .output: return 2
         case .halt: return 1
+        }
+    }
+}
+
+extension IntcodeOperator.Parameter {
+    init(_ value: Int, mode: Int = 0) {
+        switch mode {
+        case 0:
+            self = .position(value)
+        case 1:
+            self = .immediate(value)
+        default:
+            fatalError("invalid mode \(mode)")
         }
     }
 }
