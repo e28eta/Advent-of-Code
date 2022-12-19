cdd8d59318ae7daecc7056a58046558d21530c7f
Dan Jackson <dan@djackson.org>
2022-12-19 13:56:45 -0800

    2022 Day Three, part two

--- 2022 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
+++ 2022 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
@@ -56,6 +56,7 @@ extension Character {
 }
 
 struct Rucksack {
+    let both: Set<Character>
     let left: Set<Character>
     let right: Set<Character>
 
@@ -63,6 +64,7 @@ struct Rucksack {
         let midpoint = contents.count / 2
         let midpointIndex = contents.index(contents.startIndex, offsetBy: midpoint)
 
+        both = Set(contents)
         left = Set(contents[..<midpointIndex])
         right = Set(contents[midpointIndex...])
     }
@@ -79,16 +81,72 @@ PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
-"""
+""".lines().map(Rucksack.init)
 
-let input = try readResourceFile("input.txt")
+let input = try readResourceFile("input.txt").lines().map(Rucksack.init)
 
 verify([
     (testInput, 157),
     (input, 7793),
 ], { input in
-    input.lines().map(Rucksack.init).reduce(0) { $0 + Int($1.duplicateItemPriority) }
+    input.reduce(0) { $0 + Int($1.duplicateItemPriority) }
 })
+
+/**
+ --- Part Two ---
+
+ As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+ For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the **only item type carried by all three Elves.** That is, if a group's badge is item type `B`, then all three Elves will have item type `B` somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+ The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+ Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is **common between all three Elves** in each group.
+
+ Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+ ```
+ vJrwpWtwJgWrhcsFMMfFFhFp
+ jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+ PmmdzqPrVvPwwTWBwg
+ ```
+
+ And the second group's rucksacks are the next three lines:
+
+ ```
+ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ ttgJtRGJQctTZtZT
+ CrZsJsPPZsGzwwsLwLmpwMDw
+ ```
+
+ In the first group, the only item type that appears in all three rucksacks is lowercase `r`; this must be their badges. In the second group, their badge item type must be `Z`.
+
+ Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (`r`) for the first group and 52 (`Z`) for the second group. The sum of these is `70`.
+
+ Find the item type that corresponds to the badges of each three-Elf group. **What is the sum of the priorities of those item types?**
+ */
+
+extension Sequence {
+    func everyThree() -> some Sequence<(Element, Element, Element)> {
+        return sequence(state: makeIterator()) { iter in
+            guard let one = iter.next(), let two = iter.next(), let three = iter.next() else { return nil }
+
+            return (one, two, three)
+        }
+    }
+}
+
+verify([
+    (testInput, 70),
+    (input, 2499),
+]) { input in
+    input.map(\.both)
+        .everyThree()
+        .compactMap {
+            $0.0.intersection($0.1).intersection($0.2).first?.priority
+        }
+        .reduce(0) { $0 + Int($1) }
+}
 
 
 //: [Next](@next)
