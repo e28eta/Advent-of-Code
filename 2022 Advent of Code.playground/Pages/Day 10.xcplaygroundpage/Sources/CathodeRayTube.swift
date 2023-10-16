d15c787136df76844e1cb39bd4b3f21d5228c752
Dan Jackson <dan@djackson.org>
2023-10-16 15:39:15 -0700

    2022 Day Ten, update part one implementation to use part 2 code

--- 2022 Advent of Code.playground/Pages/Day 10.xcplaygroundpage/Sources/CathodeRayTube.swift
+++ 2022 Advent of Code.playground/Pages/Day 10.xcplaygroundpage/Sources/CathodeRayTube.swift
@@ -38,29 +38,29 @@ public enum Pixel: CustomStringConvertible {
 }
 
 public struct CPU {
+    /// current register value
     public private(set) var register: Int = 1
+    /// current cycle count
     public private(set) var cycle: Int = 1
+    /// pixel that will be drawn during the current cycle, based on current register value
+    var pixel: Pixel {
+        let currentPixelIndex = ((cycle - 1) % CRT.columnCount)
+        let spriteLocation = (register-1)...(register+1)
 
-    private var scheduledAddition: (cycle: Int, value: Int)?
+        return (spriteLocation.contains(currentPixelIndex)
+                ? .lit
+                : .dark)
+
+    }
 
     private var instructions: any IteratorProtocol<Instruction>
+    private var scheduledAddition: (cycle: Int, value: Int)?
 
     public init(instructions: some IteratorProtocol<Instruction>) {
         self.instructions = instructions
     }
 
-    public mutating func apply(instruction: Instruction) {
-        switch instruction {
-        case .noop:
-            cycle += 1
-        case .addx(let value):
-            cycle += 2
-            register += value
-        }
-    }
-
-    public mutating func tick() -> Pixel {
-        // begin
+    public mutating func tick() {
         // check to see if we need a new instruction
         if scheduledAddition == nil,
            // read next instruction
@@ -70,23 +70,15 @@ public struct CPU {
             scheduledAddition = (cycle: cycle + 1, value: value)
         } // else continue previous addx, or noop
 
-        // draw
-        let currentPixelIndex = ((cycle - 1) % CRT.columnCount)
-        let spriteLocation = (register-1)...(register+1)
-        
-        let pixel: Pixel = (spriteLocation.contains(currentPixelIndex)
-                            ? .lit
-                            : .dark)
+        // "during cycle" steps should happen prior to call to `tick()`
+        // whether reading the register or figuring out pixel value
 
-
-        // end
+        // end of cycle updates
         if scheduledAddition?.cycle == cycle {
             register += scheduledAddition!.value
             scheduledAddition = nil
         }
         cycle += 1
-
-        return pixel
     }
 }
 
@@ -125,41 +117,25 @@ public class HandheldDevice {
     public func part1() -> Int {
         var cpu = CPU(instructions: program.instructions.makeIterator())
 
-        // just save them all, and then search for simplicty
-        let registerValues: [(Int, Int)] = program.instructions.map { instruction in
-            defer { cpu.apply(instruction: instruction) }
-            return (cpu.cycle, cpu.register)
-        } + [(cpu.cycle, cpu.register)]
-
         let targetCycles = [20, 60, 100, 140, 180, 220]
+        var signalStrengthSum = 0
 
-        return targetCycles.map { cycle in
-            HandheldDevice.findRegisterValue(at: cycle, in: registerValues) * cycle
-        }.reduce(0, +)
-    }
-
-    private static func findRegisterValue(at desiredCycle: Int, in values: [(Int, Int)]) -> Int {
-
-        let nextIndex = values.firstIndex { (cycle, _) in
-            desiredCycle < cycle
+        while cpu.cycle <= targetCycles.last! {
+            if targetCycles.contains(cpu.cycle) {
+                signalStrengthSum += (cpu.cycle * cpu.register)
+            }
+            cpu.tick()
         }
 
-        // might have run past end of program, use last value / default 1st value
-        guard let nextIndex else { return values.last?.1 ?? 1 }
-        // handle first index matching, or empty collection
-        guard nextIndex > values.startIndex else {
-            return values.first?.1 ?? 1
-        }
-        
-        let targetIndex = values.index(before: nextIndex)
-        return values[targetIndex].1
+        return signalStrengthSum
     }
 
     public func part2() -> CRT? {
         var cpu = CPU(instructions: program.instructions.makeIterator())
 
         let pixels = (0..<240).map { _ in
-            cpu.tick()
+            defer { cpu.tick() }
+            return cpu.pixel
         }
 
         return CRT(pixels: pixels)
