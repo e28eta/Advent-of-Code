13f85a2f8e089df66c21fab645348d7695352952
Dan Jackson <dan@djackson.org>
2023-10-16 19:20:14 -0700

    2022, Day 11, part one
    Spent a _bunch_ of time playing with Swift's RegexBuilder, and trying to get a clean-ish
    parse of the Monkey objects.
    
    Way uglier than parsing approaches I've used in the past, of identifying the core numbers
    and just splitting the strings on them.
    
    However, I'm mostly happy with it. The way I'm creating a Monkey from the regex match by
    passing the match to an initializer seems bad, since there's tight coupling between the
    built regexes and the method signature of the un-genericized match object. Probably should
    do the extraction in the same code that does the matching.

---
+++ 2022 Advent of Code.playground/Pages/Day 11.xcplaygroundpage/Sources/Monkey.swift
@@ -0,0 +1,311 @@
+import Foundation
+import RegexBuilder
+
+
+public typealias Item = Int
+
+public class MonkeyBusiness {
+    let debug = false
+    var monkeys: [Monkey]
+    var roundNumber = 1
+
+    public init(_ string: String) {
+        monkeys = string.matches(of: Monkey.MatchingRegex).map(Monkey.init)
+    }
+
+    public func part1() -> Int {
+        for _ in 0 ..< 20 {
+            round()
+        }
+
+        let counts = inspectionCounts()
+
+        if debug {
+            for (idx, count) in counts.enumerated() {
+                print("Monkey \(idx) inspected items \(count) times.")
+            }
+        }
+
+        return counts.sorted().suffix(2).reduce(1, *)
+    }
+
+    func round() {
+        for idx in monkeys.indices {
+            turn(idx)
+        }
+
+        if debug && (roundNumber < 10 || roundNumber % 5 == 0 ) {
+            print("After round \(roundNumber), the monkeys are holding items with these worry levels:")
+            for monkey in monkeys {
+                print("Monkey \(monkey.id):", monkey.items)
+            }
+            print()
+        }
+
+        roundNumber += 1
+    }
+
+    func turn(_ idx: Int) {
+        let currentMonkey = monkeys[idx]
+
+        for var item in currentMonkey.items {
+            item = currentMonkey.operation(item)
+            item /= 3
+            let destination = currentMonkey.test(item)
+            monkeys[destination].items.append(item)
+        }
+        
+        currentMonkey.inspectionCount += currentMonkey.items.count
+        currentMonkey.items = []
+
+        monkeys[idx] = currentMonkey
+    }
+
+    public func inspectionCounts() -> [Int] {
+        return monkeys.map(\.inspectionCount)
+    }
+}
+
+public class Monkey {
+    public let id: Int
+    public var items: [Item]
+    public let operation: Monkey.Operation
+    public let test: Monkey.Test
+    public var inspectionCount: Int = 0
+
+    init(_ match: Regex<(Substring, Int, Array<Int>, Monkey.Operation, Int, Int, Int)>.Match) {
+        id = match[Monkey.MonkeyIdRef]
+        items = match[Monkey.StartingItemsRef]
+        operation = match[Monkey.Operation.OperationRef]
+        test = Monkey.Test(testValue: match[Monkey.Test.TestDivisorRef],
+                           trueDestination: match[Monkey.Test.TrueTargetRef],
+                           falseDestination: match[Monkey.Test.FalseTargetRef])
+
+        guard id != test.trueDestination && id != test.falseDestination else {
+            fatalError("assumption that monkey never throws to itself is broken")
+        }
+    }
+}
+
+// MARK: Operation
+
+extension Monkey {
+    public struct Operation {
+        let left: Operand, op: Operator, right: Operand
+
+        public init(_ string: some StringProtocol) {
+            let trimmed = string.trimmingCharacters(in: .whitespaces)
+            guard let (left, suffix) = trimmed.splitOnce(separator: " "),
+                  let (op, right) = suffix.splitOnce(separator: " ")
+            else {
+                fatalError("malformed operation: \(string)")
+            }
+
+            self.left = Operand(left)
+            self.op = Operator(op)
+            self.right = Operand(right)
+        }
+
+        public func callAsFunction(_ value: Int) -> Int {
+            op(left.value ?? value, right.value ?? value)
+        }
+
+        enum Operator {
+            case add, multiply
+
+            init(_ string: some StringProtocol) {
+                switch string {
+                case "+": self = .add
+                case "*": self = .multiply
+                default: fatalError("unhandled operator \(string)")
+                }
+            }
+
+            func callAsFunction(_ left: Int, _ right: Int) -> Int {
+                switch self {
+                case .add: return left + right
+                case .multiply: return left * right
+                }
+            }
+        }
+
+        enum Operand {
+            case oldValue, literal(Int)
+
+            init( _ string: some StringProtocol) {
+                if string == "old" {
+                    self = .oldValue
+                } else if let value = Int(string) {
+                    self = .literal(value)
+                } else {
+                    fatalError("unhandled operand \(string)")
+                }
+            }
+
+            var value: Int? {
+                if case .literal(let int) = self {
+                    return int
+                } else {
+                    return nil
+                }
+            }
+        }
+    }
+}
+
+// MARK: Test
+
+extension Monkey {
+    public struct Test {
+        public let testValue: Int
+        public let trueDestination: Int
+        public let falseDestination: Int
+
+        init(testValue: Int, trueDestination: Int, falseDestination: Int) {
+            self.testValue = testValue
+            self.trueDestination = trueDestination
+            self.falseDestination = falseDestination
+        }
+
+        public func callAsFunction(_ value: Int) -> Int {
+            return (value % testValue == 0) ? trueDestination : falseDestination
+        }
+    }
+}
+
+// MARK: Regex
+let EnglishLocale = Locale(identifier: "en-US")
+
+public extension Monkey {
+    static let MonkeyIdRef = Reference<Int>()
+    static let StartingItemsRef = Reference<[Int]>()
+
+    static let MatchingRegex = Regex {
+        Anchor.startOfLine
+        "Monkey "
+        Capture(.localizedInteger(locale: EnglishLocale), as: MonkeyIdRef)
+        ":"
+        One(.newlineSequence)
+
+        StartingItemsRegex
+        One(.newlineSequence)
+
+        Monkey.Operation.MatchingRegex
+        One(.newlineSequence)
+
+        Monkey.Test.MatchingRegex
+        Optionally(.newlineSequence)
+    }
+
+    static let StartingItemsRegex = Regex {
+        Anchor.startOfLine
+        OneOrMore(.whitespace)
+        "Starting items: "
+        Capture(as: StartingItemsRef) {
+            ZeroOrMore {
+                One(.localizedInteger(locale: EnglishLocale))
+                ", "
+            }
+            One(.localizedInteger(locale: EnglishLocale))
+        } transform: {
+            $0.components(separatedBy: ", ").compactMap(Int.init)
+        }
+        Anchor.endOfLine
+    }
+}
+
+public extension Monkey.Operation {
+    static let OperationRef = Reference<Monkey.Operation>()
+    static let MatchingRegex = Regex {
+        Anchor.startOfLine
+        OneOrMore(.whitespace)
+        "Operation: new = "
+        Capture(as: OperationRef) {
+            Operand.MatchingRegex
+            One(.whitespace)
+            Operator.MatchingRegex
+            One(.whitespace)
+            Operand.MatchingRegex
+        } transform: {
+            Monkey.Operation($0)
+        }
+        Anchor.endOfLine
+    }
+}
+
+extension Monkey.Operation.Operator {
+    static let MatchingRegex = Regex {
+        ChoiceOf {
+            "+"
+            "*"
+        }
+    }
+}
+
+extension Monkey.Operation.Operand {
+    static let MatchingRegex = Regex {
+        ChoiceOf {
+            "old"
+            One(.localizedInteger(locale: EnglishLocale))
+        }
+    }
+}
+
+public extension Monkey.Test {
+    static let TestDivisorRef = Reference<Int>()
+    static let TrueTargetRef = Reference<Int>()
+    static let FalseTargetRef = Reference<Int>()
+
+    static let MatchingRegex = Regex {
+        Anchor.startOfLine
+        OneOrMore(.whitespace)
+        "Test: divisible by "
+        Capture(.localizedInteger(locale: EnglishLocale),
+                as: TestDivisorRef)
+        One(.newlineSequence)
+        OneOrMore(.whitespace)
+        "If true: throw to monkey "
+        Capture(.localizedInteger(locale: EnglishLocale),
+                as: TrueTargetRef)
+        One(.newlineSequence)
+        OneOrMore(.whitespace)
+        "If false: throw to monkey "
+        Capture(.localizedInteger(locale: EnglishLocale),
+                as: FalseTargetRef)
+        Anchor.endOfLine
+    }
+}
+
+// MARK: CustomStringConvertible
+
+extension Monkey.Operation: CustomStringConvertible {
+    public var description: String {
+        return "\(left) \(op) \(right)"
+    }
+}
+
+extension Monkey.Operation.Operator: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .add: return "+"
+        case.multiply: return "*"
+        }
+    }
+}
+
+extension Monkey.Operation.Operand: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .oldValue:
+            return "old"
+        case .literal(let int):
+            return String(int)
+        }
+    }
+}
+
+extension Monkey.Test: CustomStringConvertible {
+    public var description: String {
+        return "if % \(testValue) then \(trueDestination) else \(falseDestination)"
+    }
+}
