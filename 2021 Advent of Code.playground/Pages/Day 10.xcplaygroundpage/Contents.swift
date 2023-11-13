8630977b735dafe41aa573fd65192bdfd7d5352d
Dan Jackson <dan@djackson.org>
2023-11-13 15:13:49 -0800

    2021 Day 10, part 2
    Separates errors from scoring, but still monolithic

--- 2021 Advent of Code.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
+++ 2021 Advent of Code.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
@@ -77,7 +77,12 @@ let testInput = """
 """
 let input = try readResourceFile("input.txt")
 
-func part1(_ string: String) -> Int {
+enum SyntaxError {
+    case illegalCharacter(Int)
+    case incomplete(Int)
+}
+
+func parseAllLines(_ string: String) -> (p1: Int, p2: Int) {
     let lines = string.lines()
 
     let matchedPairs: [Character: Character] = [
@@ -92,10 +97,14 @@ func part1(_ string: String) -> Int {
         "}": 1197,
         ">": 25137,
     ]
+    let incompleteScore: [Character: Int] = [
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+    ]
 
-    var corruptionScore = 0
-
-nextLine: for line in lines {
+    let errors: [SyntaxError] = lines.map { line in
         var expectedStack: [Character] = []
 
         for character in line {
@@ -103,8 +112,7 @@ nextLine: for line in lines {
                 expectedStack.append(closing)
             } else if let expected = expectedStack.popLast() {
                 if expected != character {
-                    corruptionScore += illegalScore[character]!
-                    continue nextLine
+                    return SyntaxError.illegalCharacter(illegalScore[character]!)
                 } else {
                     // things are going well
                 }
@@ -116,15 +124,85 @@ nextLine: for line in lines {
 
         if expectedStack.count > 0 {
             // incomplete line
+            let score = expectedStack.reversed().reduce(0) { s, c in
+                (s * 5) + incompleteScore[c]!
+            }
+
+            return SyntaxError.incomplete(score)
+        } else {
+            fatalError("line without syntax errors? \(line)")
         }
     }
 
-    return corruptionScore
+    var corruptionScore = 0
+    var incompleteScores: [Int] = []
+
+    for error in errors {
+        switch error {
+        case .illegalCharacter(let score):
+            corruptionScore += score
+        case .incomplete(let score):
+            incompleteScores.append(score)
+        }
+    }
+    incompleteScores.sort()
+
+    return (p1: corruptionScore, p2: incompleteScores[incompleteScores.count / 2])
 }
 
 verify([
     (testInput, 26397),
     (input, 323613),
-], part1)
+], { parseAllLines($0).p1 })
+
+/**
+ # --- Part Two ---
+
+ Now, discard the corrupted lines. The remaining lines are **incomplete.**
+
+ Incomplete lines don't have any incorrect characters - instead, they're missing some closing characters at the end of the line. To repair the navigation subsystem, you just need to figure out the **sequence of closing characters** that complete all open chunks in the line.
+
+ You can only use closing characters (`)`, `]`, `}`, or `>`), and you must add them in the correct order so that only legal pairs are formed and all chunks end up closed.
+
+ In the example above, there are five incomplete lines:
+
+ - `[({(<(())[]>[[{[]{<()<>>` - Complete by adding `}}]])})]`.
+ - `[(()[<>])]({[<{<<[]>>(` - Complete by adding `)}>]})`.
+ - `(((({<>}<{<{<>}{[]{[]{}` - Complete by adding `}}>}>))))`.
+ - `{<[[]]>}<{[{[{[]{()[[[]` - Complete by adding `]]}}]}]}>`.
+ - `<{([{{}}[<[[[<>{}]]]>[]]` - Complete by adding `])}>`.
+
+ Did you know that autocomplete tools **also** have contests? It's true! The score is determined by considering the completion string character-by-character. Start with a total score of `0`. Then, for each character, multiply the total score by 5 and then increase the total score by the point value given for the character in the following table:
+
+ - `)`: `1` point.
+ - `]`: `2` points.
+ - `}`: `3` points.
+ - `>`: `4` points.
+
+ So, the last completion string above - `])}>` - would be scored as follows:
+
+ - Start with a total score of `0`.
+ - Multiply the total score by 5 to get `0`, then add the value of `]` (2) to get a new total score of `2`.
+ - Multiply the total score by 5 to get `10`, then add the value of `)` (1) to get a new total score of `11`.
+ - Multiply the total score by 5 to get `55`, then add the value of `}` (3) to get a new total score of `58`.
+ - Multiply the total score by 5 to get `290`, then add the value of `>` (4) to get a new total score of `294`.
+
+ The five lines' completion strings have total scores as follows:
+
+ - `}}]])})]` - `288957` total points.
+ - `)}>]})` - `5566` total points.
+ - `}}>}>))))` - `1480781` total points.
+ - `]]}}]}]}>` - `995444` total points.
+ - `])}>` - `294` total points.
+
+ Autocomplete tools are an odd bunch: the winner is found by **sorting** all of the scores and then taking the **middle** score. (There will always be an odd number of scores to consider.) In this example, the middle score is `288957` because there are the same number of scores smaller and larger than it.
+
+ Find the completion string for each incomplete line, score the completion strings, and sort the scores. **What is the middle score?**
+ */
+
+verify([
+    (testInput, 288957),
+    (input, 3103006161),
+], { parseAllLines($0).p2 })
 
 //: [Next](@next)
