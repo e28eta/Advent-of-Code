60e61de58036e4c34bbea043b720d7d46815891c
Dan Jackson <dan@djackson.org>
2023-12-01 23:06:09 -0800

    2023, Day 2, part 2
    That's more like what I expected from the beginning of AoC 😀

--- 2023 Advent of Code.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
+++ 2023 Advent of Code.playground/Pages/Day 2.xcplaygroundpage/Contents.swift
@@ -48,6 +48,8 @@ struct Game {
     // each round, color to count
     let rounds: [[String: Int]]
 
+    let totalCubesDrawn: [String: Int]
+
     init?(_ string: some StringProtocol) {
         guard let (gm, rounds) = string.splitOnce(separator: ": "),
               let (_, idStr) = gm.splitOnce(separator: " "),
@@ -65,6 +67,13 @@ struct Game {
 
         }
 
+        self.totalCubesDrawn = self.rounds.reduce([:], { cubeHash, round in
+
+            return round.reduce(into: cubeHash) {
+                // only keep most cubes seen of this color in a round
+                $0[$1.key] = max($1.value, $0[$1.key, default: 0])
+            }
+        })
     }
 }
 
@@ -77,27 +86,13 @@ func part1(_ string: String) -> Int {
         "blue": 14,
     ]
 
-    let totalCubesDrawn = games.reduce(into: [:]) { gameHash, game in
-        gameHash[game.id] = game.rounds.reduce([:], { cubeHash, round in
-
-            return round.reduce(into: cubeHash) {
-                // only keep most cubes seen of this color in a round
-                $0[$1.key] = max($1.value, $0[$1.key, default: 0])
-            }
-        })
-    }
-
-    let possible = totalCubesDrawn.filter {
-        let (id, cubes) = $0
-
-        return cubes.allSatisfy {
+    return games.filter { game in
+        return game.totalCubesDrawn.allSatisfy {
             let (color, count) = $0
             return limit[color, default: .max] >= count
         }
-    }
-
-    return possible.reduce(0) {
-        $0 + $1.key
+    }.reduce(0) {
+        $0 + $1.id
     }
 }
 
@@ -105,5 +100,46 @@ verify([
     (testInput, 8),
     (input, 2076)
 ], part1)
+
+/**
+ # --- Part Two ---
+
+ The Elf says they've stopped producing snow because they aren't getting any **water!** He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+ As you continue your walk, the Elf poses a second question: in each game you played, what is the **fewest number of cubes of each color** that could have been in the bag to make the game possible?
+
+ Again consider the example games from earlier:
+
+ ```
+ Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+ ```
+
+ - In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+ - Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+ - Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+ - Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+ - Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+
+ The **power** of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is `48`. In games 2-5 it was `12`, `1560`, `630`, and `36`, respectively. Adding up these five powers produces the sum `2286`.
+
+ For each game, find the minimum set of cubes that must have been present. **What is the sum of the power of these sets?**
+ */
+
+func part2(_ string: String) -> Int {
+    let games = string.lines().compactMap(Game.init)
+
+
+    return games.reduce(0) { sum, game in
+        sum + game.totalCubesDrawn.reduce(1) {
+            $0 * $1.value
+        }
+    }
+}
+
+verify([(testInput, 2286), (input, 70950)], part2)
 
 //: [Next](@next)
