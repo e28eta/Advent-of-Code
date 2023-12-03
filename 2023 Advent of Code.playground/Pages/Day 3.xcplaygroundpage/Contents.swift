ecf24947b563e33823df6ee3fb8b9b1128a01ec4
Dan Jackson <dan@djackson.org>
2023-12-02 21:45:41 -0800

    2023, Day 3, part 2

--- 2023 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
+++ 2023 Advent of Code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
@@ -52,9 +52,9 @@ let testInput = """
 let input = try readResourceFile("input.txt")
 
 extension Grid where Element == Character {
-    public func numbersAndNeighboringSymbols() -> some Sequence<(Int, [Character])> {
+    public func numbersAndNeighboringSymbols() -> some Sequence<(Int, Set<GridIndex>)> {
         return (0 ..< height).flatMap { rowIdx in
-            var result: [(Int, [Character])] = []
+            var result: [(Int, Set<GridIndex>)] = []
             var partialNumber = 0
             var adjacentSymbols = Set<Index>()
 
@@ -72,9 +72,7 @@ extension Grid where Element == Character {
 
                     adjacentSymbols.formUnion(neighbors)
                 } else if partialNumber > 0 {
-                    result.append((partialNumber, adjacentSymbols.map({
-                        self[$0]
-                    })))
+                    result.append((partialNumber, adjacentSymbols))
                     adjacentSymbols = Set()
                     partialNumber = 0
                 }
@@ -102,5 +100,68 @@ verify([
     (testInput, 4361),
     (input, 532428)
 ], part1)
+
+/**
+ # --- Part Two ---
+
+ The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+
+ You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+
+ Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+ The missing part wasn't the only issue - one of the gears in the engine is wrong. A **gear** is any `*` symbol that is adjacent to **exactly two part numbers.** Its **gear ratio** is the result of multiplying those two numbers together.
+
+ This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+ Consider the same engine schematic again:
+
+ ```
+ 467..114..
+ ...*......
+ ..35..633.
+ ......#...
+ 617*......
+ .....+.58.
+ ..592.....
+ ......755.
+ ...$.*....
+ .664.598..
+ ```
+
+ In this schematic, there are **two** gears. The first is in the top left; it has part numbers `467` and `35`, so its gear ratio is `16345`. The second gear is in the lower right; its gear ratio is `451490`. (The `*` adjacent to `617` is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces `467835`.
+
+  **What is the sum of all of the gear ratios in your engine schematic?**
+ */
+
+func part2(_ string: String) -> Int {
+    let asCharacters = string.lines().map { Array($0) }
+    let grid = Grid<Character>(asCharacters, connectivity: GridConnectivity.eightWay)
+
+    let gears = grid.numbersAndNeighboringSymbols()
+        .reduce(into: [GridIndex: [Int]]()) { gears, numSyms in
+
+            let asterisks = numSyms.1.filter { idx in
+                grid[idx] == "*"
+            }
+
+            for idx in asterisks {
+                gears[idx, default: []].append(numSyms.0)
+            }
+        }
+        .filter {
+            $0.value.count > 1
+        }
+        .mapValues {
+            $0.reduce(1, *)
+        }
+
+    return gears.reduce(0) { $0 + $1.value }
+}
+
+verify([
+    (testInput, 467835),
+    (input, 84051670)
+], part2)
 
 //: [Next](@next)
