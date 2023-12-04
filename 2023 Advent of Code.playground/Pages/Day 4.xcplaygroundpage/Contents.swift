0bbf7296883f5fb00daa63e3abaeb176bce82422
Dan Jackson <dan@djackson.org>
2023-12-03 21:38:59 -0800

    2023, Day 4, part 2
    Fun! I don't know if this is the most straightforward way to think about it, but I'm okay
    with it

--- 2023 Advent of Code.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
+++ 2023 Advent of Code.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
@@ -56,6 +56,7 @@ struct Scratchcard {
     let id: Int
     let winningNums: Set<Int>
     let numbers: [Int]
+    let numberOfWins: Int
 
     init(_ line: String) {
         guard let (prefix, allNums) = line.splitOnce(separator: ":"),
@@ -64,7 +65,7 @@ struct Scratchcard {
             fatalError("badly formatted line? \(line)")
         }
 
-        var scanner = Scanner(string: String(prefix))
+        let scanner = Scanner(string: String(prefix))
         guard scanner.scanString("Card") != nil,
               let id = scanner.scanInt()
         else {
@@ -74,6 +75,8 @@ struct Scratchcard {
         self.id = id
         self.winningNums = Set(winning.components(separatedBy: .whitespaces).compactMap(Int.init))
         self.numbers = have.components(separatedBy: .whitespaces).compactMap(Int.init)
+
+        self.numberOfWins = numbers.filter(winningNums.contains).count
     }
 }
 
@@ -81,11 +84,7 @@ func part1(_ string: String) -> Int {
     let cards = string.lines().map(Scratchcard.init)
 
     return cards.reduce(0) { sum, card in
-        return sum + card.numbers
-            .filter(card.winningNums.contains)
-            .reduce(0) { p, num in
-                p == 0 ? 1 : p * 2
-            }
+        return sum + (card.numberOfWins == 0 ? 0 : (1 << (card.numberOfWins - 1)))
     }
 }
 
@@ -93,5 +92,73 @@ verify([
     (testInput, 13),
     (input, 21158),
 ], part1)
+
+/**
+ # --- Part Two ---
+
+ Just as you're about to report your findings to the Elf, one of you realizes that the rules have actually been printed on the back of every card this whole time.
+
+ There's no such thing as "points". Instead, scratchcards only cause you to **win more scratchcards** equal to the number of winning numbers you have.
+
+ Specifically, you win **copies** of the scratchcards below the winning card equal to the number of matches. So, if card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
+
+ Copies of scratchcards are scored like normal scratchcards and have the **same card number** as the card they copied. So, if you win a copy of card 10 and it has 5 matching numbers, it would then win a copy of the same cards that the original card 10 won: cards 11, 12, 13, 14, and 15. This process repeats until none of the copies cause you to win any more cards. (Cards will never make you copy a card past the end of the table.)
+
+ This time, the above example goes differently:
+
+ ```
+ Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+ Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+ ```
+
+ - Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+ - Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+ - Your copy of card 2 also wins one copy each of cards 3 and 4.
+ - Your four instances of card 3 (one original and three copies) have two matching numbers, so you win four copies each of cards 4 and 5.
+ - Your eight instances of card 4 (one original and seven copies) have one matching number, so you win eight copies of card 5.
+ - Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+ - Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
+
+ Once all of the originals and copies have been processed, you end up with `1` instance of card 1, `2` instances of card 2, `4` instances of card 3, `8` instances of card 4, `14` instances of card 5, and `1` instance of card 6. In total, this example pile of scratchcards causes you to ultimately have `30` scratchcards!
+
+ Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, **how many total scratchcards do you end up with?**
+ */
+
+extension Array {
+    // not a built-in function on array, but I like these semantics
+    mutating func popFirst() -> Element? {
+        if isEmpty { return nil }
+        return removeFirst()
+    }
+}
+
+func part2(_ string: String) -> Int {
+    let cards = string.lines().map(Scratchcard.init)
+
+    var additionalCopies = [Int]()
+
+    return cards.reduce(0) { sum, card in
+        let numCopiesThisCard = 1 + (additionalCopies.popFirst() ?? 0)
+
+        for i in (0 ..< card.numberOfWins) {
+            if i < additionalCopies.endIndex {
+                additionalCopies[i] += numCopiesThisCard
+            } else {
+                additionalCopies.append(numCopiesThisCard)
+            }
+        }
+
+        return sum + numCopiesThisCard
+    }
+}
+
+verify([
+    (testInput, 30),
+    (input, 6050769),
+], part2)
 
 //: [Next](@next)
