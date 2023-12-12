7d4aab65be9aa3cee3aa47368c45bbff7f0e2a7e
Dan Jackson <dan@djackson.org>
2023-12-12 12:56:43 -0800

    2023, Day 7, part 2

--- 2023 Advent of Code.playground/Pages/Day 7.xcplaygroundpage/Sources/CamelCards.swift
+++ 2023 Advent of Code.playground/Pages/Day 7.xcplaygroundpage/Sources/CamelCards.swift
@@ -3,7 +3,7 @@ import Foundation
 public enum CardRank: Character, CaseIterable {
     case Ace = "A", King = "K", Queen = "Q", Jack = "J", Ten = "T",
          Nine = "9", Eight = "8", Seven = "7", Six = "6",
-         Five = "5", Four = "4", Three = "3", Two = "2"
+         Five = "5", Four = "4", Three = "3", Two = "2", Joker = "X"
 }
 
 public enum HandType {
@@ -28,12 +28,16 @@ public struct CamelCardHand {
     let type: HandType
     let bid: Int
 
-    init(_ line: some StringProtocol) {
+    init(_ line: some StringProtocol, useJokers: Bool = false) {
         guard let (cardsStr, bidStr) = line.splitOnce(separator: " ") else {
             fatalError("missing space")
         }
 
-        let cards = cardsStr.compactMap(CardRank.init)
+        let cards = cardsStr.compactMap({
+            $0 == "J" && useJokers
+            ? CardRank.Joker
+            : CardRank(rawValue: $0)
+        })
 
         guard let bid = Int(bidStr),
               cards.count == 5 else {
@@ -43,13 +47,23 @@ public struct CamelCardHand {
         self.bid = bid
         self.cards = cards
 
-        let rankFrequencies = cards.reduce(into: [:]) { h, r in
+        var rankFrequencies = cards.reduce(into: [:]) { h, r in
             h[r, default: 0] += 1
         }
+        let jokerCount = rankFrequencies.removeValue(forKey: .Joker) ?? 0
+        var frequencies = rankFrequencies
             .values
             .sorted(by: >)
 
-        self.type = switch rankFrequencies {
+        if frequencies.isEmpty {
+            // 5 jokers, crazy!
+            frequencies.append(jokerCount)
+        } else {
+            // always use Jokers as most common card
+            frequencies[0] += jokerCount
+        }
+
+        self.type = switch frequencies {
         case [5]: .FiveOfAKind
         case [4, 1]: .FourOfAKind
         case [3, 2]: .FullHouse
@@ -63,9 +77,9 @@ public struct CamelCardHand {
 }
 
 extension CamelCards {
-    public init(_ string: String) {
+    public init(_ string: String, useJokers: Bool = false) {
         rankedHands = string.lines()
-            .map(CamelCardHand.init)
+            .map({ CamelCardHand($0, useJokers: useJokers) })
             .sorted(by: <)
     }
 }
