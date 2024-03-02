b5ac06ccfc2ccacb49d16c48917c2d92c72e8ba2
Dan Jackson <dan@djackson.org>
2024-03-02 12:10:36 -0800

    2023, Day 8, part 2 partial work from December

--- 2023 Advent of Code.playground/Pages/Day 8.xcplaygroundpage/Contents.swift
+++ 2023 Advent of Code.playground/Pages/Day 8.xcplaygroundpage/Contents.swift
@@ -133,4 +133,119 @@ ZZZ = (ZZZ, ZZZ)
     Network($0).part1()
 }
 
+/**
+ # --- Part Two ---
+
+ The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel follow the instructions, but you've barely left your starting position. It's going to take **significantly more steps** to escape!
+
+ What if the map isn't for people - what if the map is for **ghosts?** Are ghosts even bound by the laws of spacetime? Only one way to find out.
+
+ After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names ending in `A` is equal to the number ending in `Z`! If you were a ghost, **you'd probably just start at every node that ends with** `A` and follow all of the paths at the same time until they all simultaneously end up at nodes that end with `Z`.
+
+ For example:
+
+ ```
+ LR
+
+ 11A = (11B, XXX)
+ 11B = (XXX, 11Z)
+ 11Z = (11B, XXX)
+ 22A = (22B, XXX)
+ 22B = (22C, 22C)
+ 22C = (22Z, 22Z)
+ 22Z = (22B, 22B)
+ XXX = (XXX, XXX)
+ ```
+
+ Here, there are two starting nodes, `11A` and `22A` (because they both end with A). As you follow each left/right instruction, use that instruction to **simultaneously** navigate away from both nodes you're currently on. Repeat this process until **all** of the nodes you're currently on end with `Z`. (If only some of the nodes you're on end with `Z`, they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+
+ - Step 0: You are at `11A` and `22A`.
+ - Step 1: You choose all of the **left** paths, leading you to `11B` and `22B`.
+ - Step 2: You choose all of the **right** paths, leading you to `11Z` and `22C`.
+ - Step 3: You choose all of the **left** paths, leading you to `11B` and `22Z`.
+ - Step 4: You choose all of the **right** paths, leading you to `11Z` and `22B`.
+ - Step 5: You choose all of the **left** paths, leading you to `11B` and `22C`.
+ - Step 6: You choose all of the **right** paths, leading you to `11Z` and `22Z`.
+
+ So, in this example, you end up entirely on nodes that end in `Z` after **6** steps.
+
+ Simultaneously start on every node that ends with `A`. How many steps does it take before you're only on nodes that end with `Z`?
+ */
+
+extension Network {
+    public func part2() -> Int {
+        var currentNodes = nodes.keys
+            .filter {
+                $0.suffix(1) == "A"
+            }
+            .map {
+                nodes[$0]!
+            }
+
+        for n in currentNodes {
+            print("start node \(n.label)")
+        }
+        for n in nodes.keys.filter({$0.suffix(1) == "Z" }) {
+            print("possible end node \(n)")
+        }
+
+        /*
+         Looks like this is computationally infeasible
+
+         New idea: for each starting node, figure out the
+         cycle lengths where it reaches possible end nodes
+
+
+         Maybe a single cycle, maybe multiple?
+
+         Seems familiar to previous problem I've recently
+         done, where 2nd time through cycled directions
+         has a different start point,
+         but maybe some point it'll converge?
+
+         for each (node + cycle of directions) -> end node
+         with potential "goal nodes" in the middle
+
+         figure out equation(s) / sequence of numbers for
+         each of the 6 starting nodes independently.
+         There's a total of 6 ending nodes, problem statement says they're equal in number
+         Doesn't specifically say they are disjoint
+
+         and then LCM across the collection to find shortest
+         */
+
+        for (offset, dir) in directions.cycled().enumerated() {
+
+            currentNodes = currentNodes.map {
+                nodes[$0[dir]]!
+            }
+
+            if currentNodes.allSatisfy({ $0.label.suffix(1) == "Z" }) {
+                return offset + 1
+            }
+        }
+
+        // infinite directions, dead code
+        return -1
+    }
+}
+
+verify([
+    ("""
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+""", 6),
+    (input, 0),
+]) {
+    Network($0).part2()
+}
+
 //: [Next](@next)
