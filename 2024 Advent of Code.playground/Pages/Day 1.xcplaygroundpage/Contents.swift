76e56af2e729c25c97cd262c85993317c76941f3
Dan Jackson <dan@djackson.org>
2024-12-01 16:42:58 -0800

    2024 Day 1, part two

--- 2024 Advent of Code.playground/Pages/Day 1.xcplaygroundpage/Contents.swift
+++ 2024 Advent of Code.playground/Pages/Day 1.xcplaygroundpage/Contents.swift
@@ -58,8 +58,8 @@ let testInput = """
 """
 let input = try readResourceFile("input.txt")
 
-func part1(_ input: String) -> Int {
-    let (left, right): ([Int], [Int]) = input.lines().reduce(into: ([], [])) { result, line in
+func parse(_ input: String) -> ([Int], [Int]) {
+    return input.lines().reduce(into: ([], [])) { result, line in
         if let (l, r) = line.splitOnce(separator: "   "),
            let l = Int(l),
            let r = Int(r) {
@@ -67,6 +67,10 @@ func part1(_ input: String) -> Int {
             result.1.append(r)
         }
     }
+}
+
+func part1(_ input: String) -> Int {
+    let (left, right) = parse(input)
 
     return zip(left.sorted(), right.sorted()).map {
         abs($1 - $0)
@@ -76,7 +80,53 @@ func part1(_ input: String) -> Int {
 verify([(testInput, 11), (input, 2367773)], part1)
 
 /**
- 
+ # --- Part Two ---
+
+ Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very different.
+
+ Or are they?
+
+ The Historians can't agree on which group made the mistakes **or** how to read most of the Chief's handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted handwriting.
+
+ This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total **similarity score** by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+ Here are the same example lists again:
+
+ ```
+ 3   4
+ 4   3
+ 2   5
+ 1   3
+ 3   9
+ 3   3
+ ```
+
+ For these example lists, here is the process of finding the similarity score:
+
+  - The first number in the left list is `3`. It appears in the right list three times, so the similarity score increases by `3 * 3 = 9`.
+  - The second number in the left list is `4`. It appears in the right list once, so the similarity score increases by `4 * 1 = 4`.
+  - The third number in the left list is `2`. It does not appear in the right list, so the similarity score does not increase `(2 * 0 = 0)`.
+  - The fourth number, `1`, also does not appear in the right list.
+  - The fifth number, `3`, appears in the right list three times; the similarity score increases by `9`.
+  - The last number, `3`, appears in the right list three times; the similarity score again increases by `9`.
+
+ So, for these example lists, the similarity score at the end of this process is `31` (`9 + 4 + 0 + 0 + 9 + 9`).
+
+ Once again consider your left and right lists. **What is their similarity score?**
  */
+
+func part2(_ input: String) -> Int {
+    let (left, right) = parse(input)
+
+    let frequency = right.reduce(into: [:]) { freq, value in
+        freq[value, default: 0] += 1
+    }
+
+    return left.reduce(0) { sum, value in
+        sum + (frequency[value, default: 0] * value)
+    }
+}
+
+verify([(testInput, 31), (input, 21271939)], part2)
 
 //: [Next](@next)
