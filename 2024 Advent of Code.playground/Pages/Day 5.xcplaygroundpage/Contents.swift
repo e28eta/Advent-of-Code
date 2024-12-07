94781bba881f1fc069b2c2ac1a8229b49eafd785
Dan Jackson <dan@djackson.org>
2024-12-06 17:30:42 -0800

    2024 Day 5, part two

--- 2024 Advent of Code.playground/Pages/Day 5.xcplaygroundpage/Contents.swift
+++ 2024 Advent of Code.playground/Pages/Day 5.xcplaygroundpage/Contents.swift
@@ -184,4 +184,61 @@ verify([
     return PageQueue($0)?.part1() ?? 0
 }
 
+/**
+ # --- Part Two ---
+
+ While the Elves get to work printing the correctly-ordered updates, you have a little time to fix the rest of them.
+
+ For each of the **incorrectly-ordered updates**, use the page ordering rules to put the page numbers in the right order. For the above example, here are the three incorrectly-ordered updates and their correct orderings:
+
+ - `75,97,47,61,53` becomes `97,75,47,61,53`.
+ - `61,13,29` becomes `61,29,13`.
+ - `97,13,75,29,47` becomes `97,75,47,29,13`.
+
+ After taking **only the incorrectly-ordered updates** and ordering them correctly, their middle page numbers are `47`, `29`, and `47`. Adding these together produces `123`.
+
+ Find the updates which are not in the correct order. **What do you get if you add up the middle page numbers after correctly ordering just those updates?**
+ */
+
+extension PageQueue {
+    func part2() -> Int {
+        var sum = 0
+
+        for update in incorrectlyOrderedUpdates {
+            // idea: set of pages that need to be printed
+            // find which page could be printed without violating a rule,
+            // by subtracting all of the successor page numbers for the pages left to be printed
+            // should be one or more pages (probably one) at each step
+            // honestly could just go halfway through and quit
+
+            var printedOrder = [Int]()
+            var remainingToBePrinted = Set(update)
+
+            while printedOrder.count <= (update.count / 2) {
+                var nextPageCandidates = remainingToBePrinted
+                for page in remainingToBePrinted {
+                    nextPageCandidates.subtract(rules[page, default: Set()])
+                }
+
+                guard nextPageCandidates.count > 0, let nextPage = nextPageCandidates.first else {
+                    fatalError("No candidates left")
+                }
+                printedOrder.append(nextPage)
+                remainingToBePrinted.remove(nextPage)
+            }
+
+            sum += printedOrder[update.count / 2]
+        }
+
+        return sum
+    }
+}
+
+verify([
+    (testInput, 123),
+    (input, 4713)
+]) {
+    return PageQueue($0)?.part2() ?? 0
+}
+
 //: [Next](@next)
